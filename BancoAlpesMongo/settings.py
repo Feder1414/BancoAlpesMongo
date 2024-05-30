cfa9c281155fdcf8c97d74aab7a7a23bb19cadf1
maurive2014 <111020376+maurive2014@users.noreply.github.com>
2024-05-30 14:24:05 -0500

    Update settings.py

--- BancoAlpesMongo/settings.py
+++ BancoAlpesMongo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(-c8h26((i6j2$im-w#b5_=^ofkol^$s=8har0)-0gzbyk(fde
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
